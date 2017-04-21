cf727062a307534124dca37005b684d136d7230f
shenyiya <ah.shenyiya@163.com>
2017-04-21 16:27:41 +0800

    完善职位详情页面

--- react-native-content/pages/JobDetail.js
+++ react-native-content/pages/JobDetail.js
@@ -26,7 +26,9 @@ import TitleView from '../ui/TitleView';
 var deviceWidth = Utils.getScreenWidth();
 var k = Utils.getAutoScaleHeight();
 
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/EvilIcons';
+import Icon1 from 'react-native-vector-icons/FontAwesome';
+import Icon2 from 'react-native-vector-icons/Ionicons';
 
 export default class JobDetail extends Component {
 
@@ -63,43 +65,108 @@ export default class JobDetail extends Component {
 
     render() {
         return (
-            <View style={{backgroundColor: "#f0f5f1", flex: 1}}>
+            <View style={{backgroundColor: "#fff", flex: 1}}>
                 <TitleView title="职位详情" {...this.props} showShare={true}/>
-                <View style={styles.container}>
-                    <Tabs defaultActiveKey="1" onChange={this.callback} onTabClick={this.handleTabClick}
-                          swipeable={false} animated={true}>
-                        <TabPane tab="选项卡一" key="1">
-                            <View style={{
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                backgroundColor: '#fff',
-                                height: 500 * k
+                <ScrollView style={{flex: 1}} showsVerticalScrollIndicator={false}>
+                    <View style={{backgroundColor: "#fff"}}>
+                        <Text style={styles.title} numberOfLines={1}>我是标题我是标题我是标题我是标题我是标题abcdeeffg</Text>
+                        <View
+                            style={{
+                                marginLeft: 36 * k,
+                                marginTop: 20 * k,
+                                flexDirection: "row",
+                                backgroundColor: "#fff",
+                                marginBottom: 20 * k
                             }}>
-                                <Text> 选项卡一内容</Text>
-                            </View>
-                        </TabPane>
-                        <TabPane tab="选项卡二" key="2">
-                            <View style={{
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                backgroundColor: '#fff',
-                                height: 500 * k
-                            }}>
-                                <Text>选项卡2222内容</Text>
-                            </View>
-                        </TabPane>
-                        <TabPane tab="选项卡三" key="3">
-                            <View style={{
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                backgroundColor: '#fff',
-                                height: 500 * k
-                            }}>
-                                <Text> 选项卡33333内容</Text>
-                            </View>
-                        </TabPane>
-                    </Tabs>
-                    <WhiteSpace />
+                            <Icon1 name="rmb" size={30 * k} color="#26B484" style={{marginTop: 2 * k}}/>
+                            <Text style={{marginLeft: 5 * k, color: "#666666", fontSize: 24 * k}}>200/天</Text>
+                            <Icon name="location" size={40 * k} color="#26B484"
+                                  style={{marginLeft: 60 * k, marginTop: 0 * k}}/>
+                            <Text style={{marginLeft: 5 * k, color: "#666666", fontSize: 24 * k}}>天安门</Text>
+                            <Icon name="calendar" size={40 * k} color="#26B484"
+                                  style={{marginLeft: 60 * k, marginTop: 2 * k}}/>
+                            <Text style={{marginLeft: 5 * k, color: "#666666", fontSize: 24 * k}}>周五</Text>
+                        </View>
+                    </View>
+                    <View style={{height: 25 * k, backgroundColor: "#f0f5f1", width: deviceWidth}}/>
+                    <View style={{backgroundColor: "#fff", marginTop: 25 * k, marginBottom: 25 * k}}>
+                        <Text style={styles.title} numberOfLines={1}>公司名公司名公司名公司名公司名公司名</Text>
+                        <Text style={{
+                            marginLeft: 36 * k,
+                            color: "#999999",
+                            fontSize: 26 * k,
+                            marginRight: 36 * k,
+                            marginBottom: 20 * k,
+                            marginTop: 20 * k,
+                            lineHeight: 20,
+                        }}>可以看到iphone
+                            6的宽度为 375pt，
+                            对应了上边的375，由此可见react的单位为pt。 那如何获取实际的像素尺寸呢？ 这对图片的高清化很重要，
+                            如果我的图片大小为100*100 px. 设置宽度为100 * 100</Text>
+                    </View>
+                    <View style={{height: 25 * k, backgroundColor: "#f0f5f1", width: deviceWidth}}/>
+                    <View style={styles.container}>
+                        <Tabs defaultActiveKey="1" onChange={this.callback} onTabClick={this.handleTabClick}
+                              swipeable={false} animated={true}
+                              underlineColor="#999"
+                              activeUnderlineColor="#26B484"
+                              textColor="#333333"
+                              activeTextColor="#26B484"
+                        >
+                            <TabPane tab="描述" key="1" style={{backgroundColor: "#fff"}}>
+                                <View style={{
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    backgroundColor: '#fff',
+                                }}>
+                                    <Text style={{marginLeft: 36 * k, marginRight: 36 * k}}>{Constant.jobDetail}</Text>
+                                </View>
+                            </TabPane>
+                            <TabPane tab="评价" key="2">
+                                <View style={{
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    backgroundColor: '#fff',
+                                    height: 500 * k
+                                }}>
+                                    <Text>选项卡2222内容</Text>
+                                </View>
+                            </TabPane>
+                        </Tabs>
+                        <WhiteSpace />
+                    </View>
+                </ScrollView>
+                <View
+                    style={{
+                        height: 130 * k, backgroundColor: "transparent", width: deviceWidth, alignItems: "center",
+                        flexDirection: "row"
+                    }}>
+                    <View style={{
+                        width: 300 * k,
+                        height: 95 * k,
+                        backgroundColor: Constant.mainColor,
+                        borderRadius: 10 * k,
+                        marginLeft: 55 * k,
+                        flexDirection: "row",
+                        alignItems: "center",
+                        justifyContent: "center"
+                    }}>
+                        <Icon2 name="ios-paper-plane-outline" size={40 * k} color="#fff"/>
+                        <Text style={{color: "#fff", fontSize: 32 * k, marginLeft: 13 * k}}>按钮1</Text>
+                    </View>
+                    <View style={{
+                        width: 300 * k,
+                        height: 95 * k,
+                        backgroundColor: "#1BABE0",
+                        borderRadius: 10 * k,
+                        marginLeft: 40 * k,
+                        flexDirection: "row",
+                        alignItems: "center",
+                        justifyContent: "center"
+                    }}>
+                        <Icon name="star" size={45 * k} color="#fff"/>
+                        <Text style={{color: "#fff", fontSize: 32 * k, marginLeft: 10 * k}}>按钮2</Text>
+                    </View>
                 </View>
             </View>
         );
@@ -108,7 +175,15 @@ export default class JobDetail extends Component {
 const styles = StyleSheet.create({
     // 页面框架
     container: {
-        flex: 1
+        flex: 1,
+        marginBottom: 30 * k
     },
+    title: {
+        fontSize: 32 * k,
+        color: "#333333",
+        marginLeft: 36 * k,
+        marginTop: 30 * k,
+        marginRight: 36 * k,
+    }
 
 });
